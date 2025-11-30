81ac69d84a3bd060ee921e2cc7ee825655ca6d69
Mr.Adam <adamn2742@gmail.com>
2025-11-30 11:40:28 +0700

    Fix: Replace HEADER_FORWARDED_HOST with HEADER_X_FORWARDED_HOST for Laravel 12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,7 +29,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(
             '*',
             Request::HEADER_X_FORWARDED_FOR   | 
-            Request::HEADER_FORWARDED_HOST    |
+            Request::HEADER_X_FORWARDED_HOST  |
             Request::HEADER_X_FORWARDED_PORT  |
             Request::HEADER_X_FORWARDED_PROTO 
         );
